4a4f173ce396f7d01f39f105c56c48823d6dbb7d
David Hara <hakuhen@gmail.com>
2013-10-27 12:55:49 -0500

    Added implementation to get all plans, and queue a plan from hipchat

--- src/bamboo/bamboo.js
+++ src/bamboo/bamboo.js
@@ -8,16 +8,23 @@ var Bamboo = function (domain) {
   }
 }
 
+/**
+ * Authenticate a user
+ *
+ * @param username
+ * @param password
+ * @param callback
+ */
 Bamboo.prototype.authenticate = function(username, password, callback) {
 
-  var loginUrl = this.domain + '/rest/api/latest/plan?os_authType=basic&os_username=' + username + '&os_password=' + password;
-  request.get(loginUrl, function(error, response, body) {
+  var uri = this.domain + '/rest/api/latest/plan?os_authType=basic&os_username=' + username + '&os_password=' + password;
+  request.get(uri, function(error, response, body) {
     if (error) {
-      console.error("Authentication Failed with request :" + loginUrl);
+      console.error('Authentication Failed');
       console.error(error);
       callback(false);
     } else if (response.statusCode === 401) {
-      console.log("Incorrect username or password for: " + username);
+      console.log('Incorrect username or password for: ' + username);
       callback(false)
     }
 
@@ -27,40 +34,67 @@ Bamboo.prototype.authenticate = function(username, password, callback) {
   });
 }
 
+/**
+ * Get all plans from bamboo.
+ *
+ * TODO: figure out a better error handling system.
+ * @param callback
+ */
 Bamboo.prototype.getPlans = function(callback) {
 
-  var plansUri = this.domain + '/rest/api/latest/plan.json'
-  request.get(plansUri, function(error, response, body) {
+  var uri = this.domain + '/rest/api/latest/plan.json'
+  request.get(uri, function(error, response, body) {
     if (error) {
-      console.error("Error getting plans.");
-      console.error("Error");
+      console.error('Error getting plans.');
+      console.error('Error');
     } else if (response.statusCode === 401) {
-      console.log("Not authenticated");
+      console.log('Not authenticated');
     }
 
     var parsedBody = JSON.parse(body);
-    var planMap = {};
-    parsedBody.plans.plan.forEach(function(plan) {
-      planMap[plan.key] = plan;
-    });
-
     if (callback) {
-      callback(planMap);
+      callback(parsedBody.plans.plan);
     }
   }).auth(this.username, this.password);
 }
 
-Bamboo.prototype.getPlan = function(projectKey, buildKey, callback) {
+/**
+ * Get all plans for a specific project.
+ *
+ * @param projectKey
+ * @param callback
+ */
+Bamboo.prototype.getPlansForProject = function(projectKey, callback) {
+  this.getPlans(function(allPlans) {
+    var projectPlans = [];
+    allPlans.forEach(function(plan) {
+      if (plan.key.lastIndexOf(projectKey, 0) === 0) {
+        projectPlans.push(plan);
+      }
+    });
+    if (callback) {
+      callback(projectPlans);
+    }
+  });
+}
 
-  var planUri = this.domain + '/rest/api/latest/plan/' + projectKey + '-' + buildKey + '.json';
+/**
+ * Get a single plan.
+ *
+ * @param planKey
+ * @param callback
+ */
+Bamboo.prototype.getPlan = function(planKey, callback) {
 
-  console.log(planUri);
-  request.get(planUri, function(error, response, body) {
+  var uri = this.domain + '/rest/api/latest/plan/' + planKey + '.json';
+
+  console.log(uri);
+  request.get(uri, function(error, response, body) {
     if (error) {
-      console.error("Error getting plans.");
-      console.error("Error");
+      console.error('Error getting plans.');
+      console.error('Error');
     } else if (response.statusCode === 401) {
-      console.log("Not authenticated");
+      console.log('Not authenticated');
     }
 
     var parsedBody = JSON.parse(body);
@@ -71,23 +105,57 @@ Bamboo.prototype.getPlan = function(projectKey, buildKey, callback) {
   }).auth(this.username, this.password);
 }
 
-Bamboo.prototype.getResult = function(projectKey, buildKey, callback) {
+/**
+ * Get a result of a plan.
+ *
+ * TODO: This method isn't ready yet.
+ *
+ * @param planKey
+ * @param callback
+ */
+Bamboo.prototype.getResult = function(planKey, callback) {
 
-  var planUri = this.domain + '/rest/api/latest/result/' + projectKey + '-' + buildKey + '.json';
+  var uri = this.domain + '/rest/api/latest/result/' + planKey + '.json';
 
-  console.log(planUri);
-  request.get(planUri, function(error, response, body) {
+  console.log(uri);
+  request.get(uri, function(error, response, body) {
     if (error) {
-      console.error("Error getting plans.");
-      console.error("Error");
+      console.error('Error getting plans.');
+      console.error('Error');
     } else if (response.statusCode === 401) {
-      console.log("Not authenticated");
+      console.log('Not authenticated');
     }
 
     var parsedBody = JSON.parse(body);
 
     if (callback) {
       callback(parsedBody.results);
+    }
+  }).auth(this.username, this.password);
+}
+
+/**
+ * Queue a build for a plan.
+ *
+ * @param planKey
+ * @param callback
+ */
+Bamboo.prototype.queueBuild = function(planKey, callback) {
+  var uri = this.domain + '/rest/api/latest/queue/' + planKey + '.json';
+
+  console.log(uri);
+  request.post(uri, function(error, response, body) {
+    if (error) {
+      console.error('Error getting plans.');
+      console.error('Error');
+    } else if (response.statusCode === 401) {
+      console.log('Not authenticated');
+    }
+
+    var parsedBody = JSON.parse(body);
+
+    if (callback) {
+      callback(parsedBody);
     }
   }).auth(this.username, this.password);
 }
--- src/yumi.js
+++ src/yumi.js
@@ -1,22 +1,146 @@
 var Bamboo = require('./bamboo/bamboo.js'),
     HipchatClient = require('node-hipchat'),
+    moment = require('moment'),
     config = require('./config.json');
 
-console.log("Starting yumi with config:" + JSON.stringify(config));
+console.log('Starting yumi with config:' + JSON.stringify(config));
 
 // TODO: Checks for required config.
 
-var bamboo = new Bamboo(config.domain),
-    hipchat = new HipchatClient(config.hipchatApiKey);
+var bamboo = new Bamboo(config.bamboo.domain),
+    hipchat = new HipchatClient(config.hipchat.apiKey);
 
-bamboo.authenticate(config.username, config.password, function(isAuthenticated) {
-  if (isAuthenticated) {
-    bamboo.getPlans(function(planMap) {
-      console.log(planMap);
-    });
+var YUMI_KEYWORD = '!yumi';
+
+var startYumi = function() {
+  // Authenticate bamboo and start polling.
+  bamboo.authenticate(config.bamboo.username, config.bamboo.password, function(isAuthenticated) {
+    if (isAuthenticated) {
+      poll();
+    }
+  });
+}
+
+/**
+ * Poll for messages and execute commands
+ * 
+ * @param fromDate a moment.js date object.
+ */
+var poll = function(fromDate) {
+  console.log('polling for messages.');
+  var params = {
+    room : config.hipchat.room,
+    date: 'recent'
   }
-});
+  hipchat.getHistory(params, function(response, error) {
+    if (error) {
+      console.log('ERROR: ' + error);
+    }
+    var messages = response.messages;
+    var unreadMessages = getUnreadMessages(messages, fromDate);
+    var mostRecentDate = getMostRecentDateFromMessages(unreadMessages);
+    searchUnreadMessagesForCommand(unreadMessages);
+    setTimeout(poll, 10000, mostRecentDate);
+  });
+}
 
-hipchat.listRooms(function(rooms) {
-  console.log(rooms);
-})
+/**
+ * Gets all messages from the given list that happened after the given date.
+ * 
+ * @param {Array} messages The list of all messages.
+ * @param lastDate A moment.js date that we want messages after.
+ * @returns {Array} The list of 'unread' messages.
+ */
+var getUnreadMessages = function(messages, lastDate) {
+  var unreadMessages = [];
+
+  if (!lastDate) {
+    return unreadMessages;
+  }
+  messages.forEach(function(message) {
+    var messageDate = moment(message.date);
+    if (messageDate > lastDate) {
+      unreadMessages.push(message);
+    }
+  });
+
+  return unreadMessages;
+}
+
+/**
+ * Gets the date of the most recently posted message.
+ *
+ * TODO: There's lost of that can be done to improve this as the messages seem to always be in order.
+ * @param messages The list of messages.
+ * @returns {*} a moment.js object.
+ */
+var getMostRecentDateFromMessages = function(messages) {
+  var mostRecentDate = null;
+
+  // If we got no messages, make now the most recent date.
+  if (!messages || messages.length === 0) {
+    return moment();
+  }
+  messages.forEach(function (message) {
+    var messageDate = moment(message.date);
+    if (mostRecentDate === null || mostRecentDate < messageDate) {
+      mostRecentDate = messageDate;
+    }
+  });
+  return mostRecentDate;
+}
+
+/**
+ * Goes through the messages and checks the text for any key words, then executes the appropriate command to bamboo.
+ *
+ * TODO: lots of clean up here.
+ *
+ * @param {Array} unreadMessages The list of unread messages.
+ */
+var searchUnreadMessagesForCommand = function(unreadMessages) {
+  unreadMessages.forEach(function(message) {
+    if (message.message.lastIndexOf(YUMI_KEYWORD, 0) === 0) {
+      if (message.message === YUMI_KEYWORD + ' show plans') {
+        bamboo.getPlansForProject(config.bamboo.project, function(plans) {
+          var messageString = '<ul>';
+          plans.forEach(function(plan) {
+            messageString = messageString + '<li>' + plan.name + ' (' + plan.key + ')</li>';
+          });
+          messageString = messageString + '</ul>';
+          var params = {
+            room: config.hipchat.room,
+            from: 'Yumi',
+            message: messageString,
+            notify: false,
+            color: 'green',
+            message_format: 'html'
+          }
+          hipchat.postMessage(params, function(response) {
+            console.log(response);
+          });
+        });
+      } else if (message.message.lastIndexOf(YUMI_KEYWORD + ' run plan', 0) === 0) {
+        var tokens = message.message.split(' ');
+        var planKey = tokens[3];
+
+        bamboo.queueBuild(planKey, function(response) {
+          var resultUrl = 'https://' + config.bamboo.domain + '/browse/' + response.buildResultKey;
+          var messageString = 'Queuing build for plan ' + planKey + '. <a href=' + resultUrl + '>View status.</a>';
+          var params = {
+            room: config.hipchat.room,
+            from: 'Yumi',
+            message: messageString,
+            notify: false,
+            color: 'green',
+            message_format: 'html'
+          }
+          hipchat.postMessage(params, function(response) {
+            console.log(response);
+          });
+        });
+      }
+    }
+  });
+}
+
+startYumi();
