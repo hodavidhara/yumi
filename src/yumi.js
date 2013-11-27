0f146b40570de49c2cb63a84b310b237ffa93435
David Hara <david.hara@vodori.com>
2013-11-27 15:23:37 -0600

    Check configuration before starting yumi.

--- src/yumi.js
+++ src/yumi.js
@@ -1,120 +1,148 @@
-var Bamboo = require('./bamboo/bamboo.js'),
-    HipchatClient = require('node-hipchat'),
-    moment = require('moment'),
-    config = require('./config.json'),
-    StringUtil = require('./util/StringUtil.js'),
-    CommandService = require('./command/CommandService.js');
+var config = require('./config.json');
 
-console.log('Starting yumi with config:' + JSON.stringify(config));
+var properlyConfigured = function() {
+    if (!config) {
+        return false;
+    }
 
-// TODO: Checks for required config.
+    if (!config.yumi || !config.bamboo || !config.hipchat) {
+        return false;
+    }
 
-var bamboo = new Bamboo(config.bamboo.domain),
-    hipchat = new HipchatClient(config.hipchat.apiKey);
+    // Yumi configuration.
+    if (!config.yumi.dbUrl) {
+        return false;
+    }
 
-var YUMI_KEYWORD = '!yumi';
+    // Bambo configuration.
+    if (!config.bamboo.domain || !config.bamboo.username || !config.bamboo.password || !config.bamboo.project) {
+        return false;
+    }
 
-var startYumi = function() {
-    console.log('Authenticating...');
+    // Hipchat configuration.
+    if (!config.hipchat.apiKey || !config.hipchat.room) {
+        return false;
+    }
 
-    // Authenticate bamboo and start polling.
-    bamboo.authenticate(config.bamboo.username, config.bamboo.password, function(error, isAuthenticated) {
-
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        if (isAuthenticated) {
-            console.log('Authenticated!');
-            poll();
-        }
-    });
+    return true;
 };
 
-/**
- * Poll for messages and execute commands
- *
- * @param fromDate a moment.js date object.
- */
-var poll = function(fromDate) {
-    console.log('polling for messages.');
-    var params = {
-        room: config.hipchat.room,
-        date: 'recent'
+if (properlyConfigured()) {
+    var Bamboo = require('./bamboo/bamboo.js'),
+        HipchatClient = require('node-hipchat'),
+        moment = require('moment'),
+        StringUtil = require('./util/StringUtil.js'),
+        CommandService = require('./command/CommandService.js');
+
+    var bamboo = new Bamboo(config.bamboo.domain),
+        hipchat = new HipchatClient(config.hipchat.apiKey);
+
+    var YUMI_KEYWORD = '!yumi';
+
+    var startYumi = function() {
+        console.log('Authenticating...');
+
+        // Authenticate bamboo and start polling.
+        bamboo.authenticate(config.bamboo.username, config.bamboo.password, function(error, isAuthenticated) {
+
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            if (isAuthenticated) {
+                console.log('Authenticated!');
+                poll();
+            }
+        });
     };
-    hipchat.getHistory(params, function(response, error) {
-        if (error) {
-            console.log('ERROR: ' + error);
+
+    /**
+     * Poll for messages and execute commands
+     *
+     * @param fromDate a moment.js date object.
+     */
+    var poll = function(fromDate) {
+        console.log('polling for messages.');
+        var params = {
+            room: config.hipchat.room,
+            date: 'recent'
+        };
+        hipchat.getHistory(params, function(response, error) {
+            if (error) {
+                console.log('ERROR: ' + error);
+            }
+            var messages = response.messages;
+            var unreadMessages = getUnreadMessages(messages, fromDate);
+            var mostRecentDate = getMostRecentDateFromMessages(unreadMessages);
+            searchUnreadMessagesForCommand(unreadMessages);
+            setTimeout(poll, 7000, mostRecentDate);
+        });
+    };
+
+    /**
+     * Gets all messages from the given list that happened after the given date.
+     *
+     * @param {Array} messages The list of all messages.
+     * @param lastDate A moment.js date that we want messages after.
+     * @returns {Array} The list of 'unread' messages.
+     */
+    var getUnreadMessages = function(messages, lastDate) {
+        var unreadMessages = [];
+
+        if (!lastDate) {
+            return unreadMessages;
         }
-        var messages = response.messages;
-        var unreadMessages = getUnreadMessages(messages, fromDate);
-        var mostRecentDate = getMostRecentDateFromMessages(unreadMessages);
-        searchUnreadMessagesForCommand(unreadMessages);
-        setTimeout(poll, 7000, mostRecentDate);
-    });
-};
+        messages.forEach(function(message) {
+            var messageDate = moment(message.date);
+            if (messageDate > lastDate) {
+                unreadMessages.push(message);
+            }
+        });
 
-/**
- * Gets all messages from the given list that happened after the given date.
- *
- * @param {Array} messages The list of all messages.
- * @param lastDate A moment.js date that we want messages after.
- * @returns {Array} The list of 'unread' messages.
- */
-var getUnreadMessages = function(messages, lastDate) {
-    var unreadMessages = [];
-
-    if (!lastDate) {
         return unreadMessages;
-    }
-    messages.forEach(function(message) {
-        var messageDate = moment(message.date);
-        if (messageDate > lastDate) {
-            unreadMessages.push(message);
+    };
+
+    /**
+     * Gets the date of the most recently posted message.
+     *
+     * TODO: There's lost of that can be done to improve this as the messages seem to always be in order.
+     * @param messages The list of messages.
+     * @returns {*} a moment.js object.
+     */
+    var getMostRecentDateFromMessages = function(messages) {
+        var mostRecentDate = null;
+
+        // If we got no messages, make now the most recent date.
+        if (!messages || messages.length === 0) {
+            return moment();
         }
-    });
+        messages.forEach(function(message) {
+            var messageDate = moment(message.date);
+            if (mostRecentDate === null || mostRecentDate < messageDate) {
+                mostRecentDate = messageDate;
+            }
+        });
+        return mostRecentDate;
+    };
 
-    return unreadMessages;
-};
+    /**
+     * Goes through the messages and checks the text for the yumi keyword, then executes the appropriate command to bamboo.
+     *
+     * @param {Array} unreadMessages The list of unread messages.
+     */
+    var searchUnreadMessagesForCommand = function(unreadMessages) {
 
-/**
- * Gets the date of the most recently posted message.
- *
- * TODO: There's lost of that can be done to improve this as the messages seem to always be in order.
- * @param messages The list of messages.
- * @returns {*} a moment.js object.
- */
-var getMostRecentDateFromMessages = function(messages) {
-    var mostRecentDate = null;
+        console.log("Searching messages for commands: " + JSON.stringify(unreadMessages));
 
-    // If we got no messages, make now the most recent date.
-    if (!messages || messages.length === 0) {
-        return moment();
-    }
-    messages.forEach(function(message) {
-        var messageDate = moment(message.date);
-        if (mostRecentDate === null || mostRecentDate < messageDate) {
-            mostRecentDate = messageDate;
-        }
-    });
-    return mostRecentDate;
-};
+        unreadMessages.forEach(function(message) {
+            if (StringUtil.startsWith(message.message, YUMI_KEYWORD)) {
+                CommandService.runCommand(message.from, message.message);
+            }
+        });
+    };
 
-/**
- * Goes through the messages and checks the text for the yumi keyword, then executes the appropriate command to bamboo.
- *
- * @param {Array} unreadMessages The list of unread messages.
- */
-var searchUnreadMessagesForCommand = function(unreadMessages) {
-
-    console.log("Searching messages for commands: " + JSON.stringify(unreadMessages));
-
-    unreadMessages.forEach(function(message) {
-        if (StringUtil.startsWith(message.message, YUMI_KEYWORD)) {
-            CommandService.runCommand(message.from, message.message);
-        }
-    });
-};
-
-startYumi();
+    startYumi();
+} else {
+    console.log('Yumi is not properly configured! Please review your config.json file!');
+}
